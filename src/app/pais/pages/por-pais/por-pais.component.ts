5a636c09b1f610e204e5b5637c64f62b4118ba1c
melissa alvarez <melissaalh24@gmail.com>
2022-10-06 15:17:52 -0500

    avances en subscribe y observables

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
 })
-export class PorPaisComponent implements OnInit {
-  constructor() {}
+export class PorPaisComponent {
+  termino: string = 'hola mundo';
+  hayError: boolean = false;
+  paises: Country[] = [];
 
-  ngOnInit(): void {}
+  constructor(private paisService: PaisService) {}
+
+  buscar() {
+    this.hayError = false;
+    this.paisService.buscarPais(this.termino).subscribe(
+      (paises) => {
+        this.paises = paises;
+      },
+      (err) => {
+        this.hayError = true;
+        this.paises = [];
+      }
+    );
+  }
 }
--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
 import { PorPaisComponent } from './pages/por-pais/por-pais.component';
+import { RouterModule } from '@angular/router';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,6 @@ import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
     PorRegionComponent,
     VerPaisComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, RouterModule],
 })
 export class PaisModule {}
